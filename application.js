12375becab574af050ee5a9a6e2172c865b95f3d
Daniel W. Lu <dandydanny@gmail.com>
2014-06-06 05:16:38 -0700

    Functional sliding in-and-out

--- application.js
+++ application.js
@@ -1,21 +1,30 @@
 $(document).ready(function() {
 	console.log("js ready");
+	// $('.body').addClass( "initial" );
+
+
 	$('#menu-button').click(function() {
-		console.log("clicked");
-		// $('.nav').toggleClass( "hidden" );
-		$('.body').removeClass( "shift-in" );
-		$('.body').addClass( "shift-out");
-		// $('.nav').removeClass( "hidden" );
-		// $('.nav').addClass( "show" );
-		// $(".nav").slideDown();
+		console.log("hamburger clicked");
+		$('.body').removeClass( "initial" );
+
+		// Special case for the very first hamburger click,
+		// Show nav, shift-out .body
+		if (!$('.body').hasClass( "shift-out" ) && !$('.body').hasClass( "shift-in" )) {
+			$('.nav').removeClass( "hidden" )
+			$('.body').addClass( "shift-out" );
+			return;
+		}
+
+		// Subsequent clicks, we'll just toggle the shift-out and shift-in
+		$('.body').toggleClass( "shift-out");
+		$('.body').toggleClass( "shift-in" );
+		
 	});
+
+
 	$('.nav').click(function() {
 		console.log("clicked while open");
-		// $('.nav').toggleClass( "hidden" );
 		$('.body').removeClass( "shift-out" );
 		$('.body').addClass( "shift-in" );
-		// $('.nav').removeClass( "show" );
-		// $('.nav').addClass( "hidden" );
-		// $(".nav").slideUp();
 	});
 });
